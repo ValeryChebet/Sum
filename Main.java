bfc58e3863dd49d7b136c840b3d64870923be718
ValeryChebet <valerychebet.ke@gmail.com>
2024-05-27 16:50:28 +0300

    My first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    static double myDouble(double x, double y, double z){
+        return x + y + z;
+    }
+            public static void main (String[]args){
+            double mySum = myDouble(10, 20, 30);
+            System.out.println(mySum);
+            }
+}
